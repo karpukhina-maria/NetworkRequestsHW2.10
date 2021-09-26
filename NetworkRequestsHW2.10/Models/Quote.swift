179fd5bb6d213c58cc9eaa6b9826f02ab03a0ce9
Maria Karpukhina <myshka_n@mail.ru>
2021-09-26 17:55:35 +0300

    update model

--- NetworkRequestsHW2.10/Models/Quote.swift
+++ NetworkRequestsHW2.10/Models/Quote.swift
@@ -5,12 +5,10 @@
 //  Created by Mary Jane on 26.09.2021.
 //
 
-import Foundation
-
 struct Quote: Decodable {
-    let body: String
-    let author: String
-    var description: String {
-        "\(body)\n\(author)"
+    let body: String?
+    let author: String?
+    var description: String? {
+        "\(body ?? "")\n\(author ?? "")"
     }
 }
