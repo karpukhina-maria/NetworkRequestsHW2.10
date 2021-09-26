989ef7e2913be2e104bf6a03638222552d4b05ed
Maria Karpukhina <myshka_n@mail.ru>
2021-09-26 16:14:32 +0300

    Initial Commit

---
+++ NetworkRequestsHW2.10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkRequestsHW2.10
+//
+//  Created by Mary Jane on 26.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
